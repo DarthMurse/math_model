8412d992a637acd36c3aef302f812724534e3ca1
murse <1807317022@qq.com>
2022-08-12 16:12:30 +0800

    Update functions in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,6 @@
 # This script will do the preprocessing of data in csv files
 
-def preprocess_csv(csv_num):
-    '''
-    The result should be stored in a numpy array of shape 
-    [total_user_number, total_attribute_number, total_time_point_number]
-    The value should be normalized according to the maximum and minimum value of the same user
-    '''
-
 def preprocess_user(csv_num, user_num):
     '''
-    The result should be saved in a numpy array of shape
-    [total_attribute_number, total_time_point_number]
+    The result should be a pandas tabular with attribute(such as '1-1-2') as rows and dates as columns
     '''
